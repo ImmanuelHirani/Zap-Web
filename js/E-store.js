e4d6f7b2d86248677f4b3a356de9bbfc9e11ae8d
immanuelHirani <gabutb688@gmail.com>
2023-11-23 13:05:46 +0700

    ::

--- js/E-store.js
+++ js/E-store.js
@@ -1,27 +1,39 @@
-const toggleShared = document.querySelectorAll('.sharedToggle');
-const cardShared = document.querySelector('.share-card');
-const closeBox = document.querySelector('.closeModal');
+const toggleShared = document.querySelectorAll(".sharedToggle");
+const cardShared = document.querySelector(".share-card");
+const closeBox = document.querySelector(".closeModal");
 const parentBox = cardShared.parentNode;
+const EstoreCategoriesLink = document.querySelectorAll(".EstoreCategoriesLink");
 
-toggleShared.forEach(othertoggle => {
-    othertoggle.addEventListener('click', (event) => {
-        event.stopPropagation(); // Mencegah event klik menyebar ke atas (ke elemen di luar sharedToggle)
-        othertoggle.classList.add('active');
-        cardShared.classList.add('modal-share-active');
-        parentBox.classList.add('box-active');
-
-        console.log(cardShared, parentBox);
+EstoreCategoriesLink.forEach((otherlink) => {
+  otherlink.addEventListener("click", () => {
+    EstoreCategoriesLink.forEach((link) => {
+      if (link !== otherlink) {
+        link.classList.remove("CategoriesLinkStore-Active");
+      }
     });
+    otherlink.classList.add("CategoriesLinkStore-Active");
+  });
 });
 
-closeBox.addEventListener('click', () => {
-    cardShared.classList.remove('modal-share-active');
-    parentBox.classList.remove('box-active');
+toggleShared.forEach((othertoggle) => {
+  othertoggle.addEventListener("click", (event) => {
+    event.stopPropagation(); // Mencegah event klik menyebar ke atas (ke elemen di luar sharedToggle)
+    othertoggle.classList.add("active");
+    cardShared.classList.add("modal-share-active");
+    parentBox.classList.add("box-active");
 
-    // Hapus kelas 'active' dari elemen-elemen 'othertoggle' yang memiliki kelas tersebut
-    toggleShared.forEach(othertoggle => {
-        if (othertoggle.classList.contains('active')) {
-            othertoggle.classList.remove('active');
-        }
-    });
+    console.log(cardShared, parentBox);
+  });
+});
+
+closeBox.addEventListener("click", () => {
+  cardShared.classList.remove("modal-share-active");
+  parentBox.classList.remove("box-active");
+
+  // Hapus kelas 'active' dari elemen-elemen 'othertoggle' yang memiliki kelas tersebut
+  toggleShared.forEach((othertoggle) => {
+    if (othertoggle.classList.contains("active")) {
+      othertoggle.classList.remove("active");
+    }
+  });
 });
